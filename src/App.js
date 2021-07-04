32afbf7ed15706d1810bb85c17369327b384dc65
andreymolaya <81398000+andreymolaya@users.noreply.github.com>
2021-07-04 16:20:21 -0500

    diseño

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
 import "./App.css";
 
 function App() {
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const [weather, setWeather] = useState("");
-  const [temperature, setTemperature] = useState(0);
-  const [cityName, setCityName] = useState("");
-
-  const savePositionToState = (position) => {
-    setLatitude(position.coords.latitude);
-    setLongitude(position.coords.longitude);
-  };
-
-  const fetchWeather = async () => {
-    try {
-      await window.navigator.geolocation.getCurrentPosition(
-        savePositionToState
-      );
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=992e8049957dd07ad12a63f273fb5ea1&units=metric`
-      );
-      setTemperature(res.data.main.temp);
-      setCityName(res.data.name);
-      setWeather(res.data.weather[0].main);
-      console.log(res.data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchWeather();
-  }, [latitude, longitude]);
-
   return (
-    <div className="app">
-      <div className="app__container">
-        <h1>{cityName}</h1>
-        <h2>{temperature}ºC</h2>
-        <h2>{weather}</h2>
-      </div>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>      
+    </React.Fragment>
   );
 }
 
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "992e8049957dd07ad12a63f273fb5ea1",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
